2c7206fb9944d3684b4e8dc879c94d54d87f1727
AmeenXnt <ameenserbots@gmail.com>
2026-03-19 01:49:38 +0530

    AstroX⚡

--- plugins/ping.js
+++ plugins/ping.js
@@ -2,14 +2,53 @@ module.exports = {
 
   command: "ping",
 
-  run: async ({ sock, m }) => {
+  run: async ({ Astro, m }) => {
 
-    await sock.sendMessage(
-      m.key.remoteJid,
-      { text: "Pong 🏓 (Powered by @eypzx/baileys)" },
-      { quoted: m }
+    var BotName = config.BOT_NAME
+        var User = config.OWNER_NAME
+        var UserNo = config.OWNER_NUMBER
+        var Version = "_Comming Soon_"
+        var Update = "_Comming Soon_"
+        var ImgUrl = ""
+        var StartM = `
+     *BOT CONNECTED✅*
+     
+     *Version:* ${Version}
+     *Bot Name:* ${BotName}
+     *Owner:* ${User}
+     *Controller:* ${UserNo}
+     *Developer:* _AmeenInt_
+     *Dev Contact:* _+916238768108_
+     *Update:* ${Update}
+     `
+   await Astro.sendMessage(
+      Astro.user.id,
+      {
+        image: { url: "https://files.catbox.moe/dfrd9b.jpg" },
+        caption: StartM,
+        title: "AstroX V2",
+        subtitle: "Multifunctional WhatsApp Bot",
+        footer: "© Powered By Team Keiko",
+        ai: true,
+        media: true,
+        interactiveButtons: [
+          {
+            name: "quick_reply",
+            buttonParamsJson: JSON.stringify({
+              display_text: "Check Speed",
+              id: ".ping"
+            })
+          },
+          {
+            name: "cta_url",
+            buttonParamsJson: JSON.stringify({
+              display_text: "Developer Contact",
+              url: "https://wa.me/916238768108?text=_Hello+AmeenInt🗣️_"
+            })
+          }
+        ]
+      }
     )
-
   }
 
 }
